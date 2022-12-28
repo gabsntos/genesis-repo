61f302c23b484febe07930fa7c17b4189bf2d154
Gabriel <gabriel545.545@gmail.com>
2022-12-28 17:21:07 -0300

    primeira subida

---
+++ FirstCsProject/TestaPessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstCsProject
+{
+    internal class TestWalk
+    {
+        public static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.walk();
+            pessoa.tellAboutYourself("Fernando", 47);
+            pessoa.findAnswerFor(10, 10);
+            pessoa.workoutForOneHour();
+            pessoa.doingPushUps(); 
+            pessoa.drawHalfPyramid();
+        }
+    }
+}
